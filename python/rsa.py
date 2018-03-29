ebf1e8d40782c1d5f7caec06cf226245c48096b3
benpankow <pankow.ben@gmail.com>
2018-03-29 16:05:09 -0500

    Fix docstrings to use double quotes

--- python/rsa.py
+++ python/rsa.py
@@ -1,7 +1,7 @@
 from utils import string_to_bytes, bytes_to_string, mult_inverse
 
 def generate_keys(p, q):
-    '''
+    """
     Given two large primes p and q, generates the requisite components of a
     RSA public and private key.
 
@@ -9,33 +9,33 @@ def generate_keys(p, q):
     ed (mod (p-1)(q-1)) = 1
 
     Returns a 3-tuple of (e, d, n)
-    '''
+    """
     e, d, n = (0, 0, 0) # Placeholder
     # TODO Your code here
     return (e, d, n)
 
 def encrypt(message, e, n):
-    '''
+    """
     Given a string message, and a public key (e, n), encrypts the string message
     and produces the bytes representing the ciphertext.
 
     E(x) = x^e (mod n)
 
     Returns ciphertext, an integer
-    '''
+    """
     ciphertext_bytes = 0 # Placeholder
     # TODO Your code here
     return ciphertext_bytes
 
 def decrypt(ciphertext_bytes, d, n):
-    '''
+    """
     Given bytes representing ciphertext, and a private key (d, n), decrypts the
     ciphertext and produces the original message.
 
     D(y) = y^d (mod n)
 
     Returns a string message
-    '''
+    """
     message = '' # Placeholder
     # TODO Your code here
     return message
--- python/utils.py
+++ python/utils.py
@@ -1,8 +1,8 @@
 def string_to_bytes(string):
-    '''
+    """
     Transforms a string of ascii text into a string of bytes representing the
     message
-    '''
+    """
     ret_bytes = 0
     for char in string:
         ret_bytes = ret_bytes << 8
@@ -10,9 +10,9 @@ def string_to_bytes(string):
     return ret_bytes
 
 def bytes_to_string(byte_str):
-    '''
+    """
     Transforms a given string of bytes into corresponding ascii text
-    '''
+    """
     ret_str = ''
     while byte_str != 0:
         ch_code = byte_str & 255
@@ -23,12 +23,12 @@ def bytes_to_string(byte_str):
 # egcd and mulinv come from:
 # https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm#Python
 def gcd(a, b):
-    '''
+    """
     Uses the Euclidian Algorithm to find the greatest commmon divisor for a and
     b.
 
     Returns the largest integer c such that c divides a and c divides b.
-    '''
+    """
     if a == 0:
         return (b, 0, 1)
     else:
@@ -36,11 +36,11 @@ def gcd(a, b):
         return (g, y - (b // a) * x, x)
 
 def mult_inverse(b, n):
-    '''
+    """
     Finds the multiplicitive inverse of b, given mod n
 
     Returns an integer c such that cb (mod n) = 1
-    '''
+    """
     g, x, _ = gcd(b, n)
     if g == 1:
         return x % n
